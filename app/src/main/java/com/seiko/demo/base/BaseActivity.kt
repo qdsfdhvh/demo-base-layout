3ad41572bbcdc39fab071b291e5eb6b2004817d0
seiko <605590140@qq.com>
2021-05-16 19:51:48 +0800

    Clean Code

--- app/src/main/java/com/seiko/demo/base/BaseActivity.kt
+++ app/src/main/java/com/seiko/demo/base/BaseActivity.kt
@@ -1,7 +1,5 @@
 package com.seiko.demo.base
 
-import android.app.Activity
-import android.content.res.Configuration
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import com.seiko.demo.AdaptScreenUtils
@@ -10,12 +8,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun getResources(): Resources {
         return AdaptScreenUtils.adaptHeight(super.getResources(), MOBILE_DESIGN_WIDTH_IN_DP)
-    }
-
-    @Suppress("NOTHING_TO_INLINE")
-    private inline fun Activity.isVertical(): Boolean {
-        return resources.configuration.orientation ==
-                Configuration.ORIENTATION_PORTRAIT
     }
 
     companion object {
